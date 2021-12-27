e6b86c7fc5f5cc5240f5092af90b73a6846c7e7f
Gamearoo Codes <gamearoo10@gmail.com>
2021-12-27 16:49:22 -0500

    v1

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ a npm module to connect to ram api easier
 ## Install
 
 install axios `npm i axios` or `yarn add axios`
+install the logs `npm i winston-console-format winston ` or `yarn add winston-console-format winston `
 
 `npm i ram-api.js` or `yarn add ram-api.js`
 
@@ -13,5 +14,27 @@ This package was more for a test but I may make a new package for each bot.
 ### Example
 
 ```javascript
-WORK IN PROGRESS
+const ramapi = require("ram-api");
+const apiv = "v3"; //v2 and up are available ram api versions note versions
+
+ramapi.apiversion(apiv); // outputs rather the api is outdated or not
+
+ramapi.apihug(apiv).then(async (data) => {
+	console.log(await data.data.url);
+});
 ```
+
+## Missing endpoints
+
+ram api endpoints can be found at https://ram.gamearoo.top/api/docs
+
+In the event that the ram api is on v4 instead of v3 you can always use axios to connect to it manually
+
+## Custom connections
+
+We currently have no way to make a connection Manually however we are working on a way in the event the ram api updates b4 we update this package
+
+## Support
+
+email : support@gamearoodev.com
+discord: https://discord.gamearoodev.com
---
+++ index.js
@@ -0,0 +1,84 @@
+const axios = require("axios");
+const { createLogger, format, transports, level } = require("winston");
+const { consoleFormat } = require("winston-console-format");
+
+const logger = createLogger({
+	level: "silly",
+	format: format.combine(
+		format.timestamp(),
+		format.ms(),
+		format.errors({ stack: true }),
+		format.splat(),
+		format.json()
+	),
+	defaultMeta: { service: "Test" },
+	transports: [
+		new transports.Console({
+			format: format.combine(
+				format.colorize({ all: true }),
+				format.padLevels(),
+				consoleFormat({
+					showMeta: true,
+					metaStrip: ["timestamp", "service"],
+					inspectOptions: {
+						depth: Infinity,
+						colors: true,
+						maxArrayLength: Infinity,
+						breakLength: 120,
+						compact: Infinity,
+					},
+				})
+			),
+		}),
+	],
+});
+exports.apihug = async function (version) {
+	if (!version.startsWith("v")) version = `v${version}`;
+	let p = new Promise(async (resolve, reject) => {
+		await axios
+			.get(`/hug`, { baseURL: `https://ram.gamearoo.top/api/${version}` })
+			.then(async function (response) {
+				resolve(response);
+			})
+			.catch((error) => {
+				return (
+					// console.log(error) &&
+					logger.error(`An error has happened ${error.response.statusText}`)
+				);
+			});
+	});
+
+	return p;
+};
+
+exports.apiversion = async function (version) {
+	if (!version.startsWith("v")) version = `v${version}`;
+	await axios
+		.get(`/version`, { baseURL: `https://ram.gamearoo.top/api/${version}` })
+		.then(async function (response) {
+			let apiversion = response.data.version;
+			let ifSupported = response.data.supported;
+			let ifOutdated = response.data.outdated;
+			let latest = response.data.latest;
+
+			if (ifOutdated) {
+				if (!ifSupported) {
+					return logger.error(
+						`${apiversion} is No longer supported the latest version is ${latest}`
+					);
+				} else {
+					return logger.warn(
+						`${apiversion} is outdated but still supported the latest version is ${latest}`
+					);
+				}
+			} else {
+				return logger.info(`${apiversion} Is the latest version`);
+			}
+		})
+		.catch((error) => {
+			return (
+				// console.log(error) &&
+				logger.error(`An error has happened ${error.response.statusText}`)
+			);
+		});
+};
