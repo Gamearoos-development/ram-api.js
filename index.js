d6d202d7ede214ef9730ea5b6e2233d0f551f68b
Gamearoo Codes <gamearoo10@gmail.com>
2022-03-25 00:22:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { date } = require("better-date.js");
+const { date } = require("better-date.js"); // better date lol
 
 const { Logger } = require("simply-logger");
 const outdated = ["v0", "v1", "v2", "v3", "v4", "v5", "v6"];
