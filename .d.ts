b7344a9e4d3189b3e062015bb1a48d6d40db3cd9
Gamearoo Codes <gamearoo10@gmail.com>
2022-10-03 02:22:49 -0400

    Create .d.ts

---
+++ .d.ts
@@ -0,0 +1,136 @@
+import { RamApiPro } from "./Endpoints/pro";
+
+declare module "ram-api.js" {
+	const Utils: Utils;
+	const RamApi: RamApi;
+	const RamApiPro: RamApiPro;
+	const RamApiBasic: RamApiBasic;
+}
+
+declare class RamApi {
+	constructor(apikey: sting, version: string);
+
+	helloAsync: (lang?: string) => {
+		lang?: sting | null;
+	};
+	_8ballAsync: (lang?: sting | null) => {
+		lang?: sting | null;
+	};
+	cuddleAsync: () => {};
+	goodmorningAsync: (lang?: string) => {
+		lang?: string;
+	};
+
+	goodnightAsync: (lang?: string) => {
+		lang?: string;
+	};
+	hugAsync: () => {};
+	kissAsync: () => {};
+	slapAsync: () => {};
+	sickAsync: () => {};
+	tiredAsync: () => {};
+	cryAsync: (lang?: string) => {
+		lang?: string;
+	};
+	laughAsync: () => {};
+	birthdayAsync: () => {};
+	version_infoAsync: () => {};
+	ratelimitAsync: () => {};
+	memeAsync: () => {};
+	catsAsync: () => {};
+	animeAsync: () => {};
+	ramAsync: () => {};
+	nekoparaAsync: () => {};
+	version_checkAsync: () => {};
+	apiinfoAsync: () => {};
+}
+declare class RamApiPro {
+	constructor(apikey: sting, version: string);
+
+	helloAsync: (lang?: string) => {
+		lang?: sting | null;
+	};
+	_8ballAsync: (lang?: sting | null) => {
+		lang?: sting | null;
+	};
+	cuddleAsync: () => {};
+	goodmorningAsync: (lang?: string) => {
+		lang?: string;
+	};
+
+	goodnightAsync: (lang?: string) => {
+		lang?: string;
+	};
+	hugAsync: () => {};
+	kissAsync: () => {};
+	slapAsync: () => {};
+	sickAsync: () => {};
+	tiredAsync: () => {};
+	cryAsync: (lang?: string) => {
+		lang?: string;
+	};
+	laughAsync: () => {};
+	birthdayAsync: () => {};
+	version_infoAsync: () => {};
+	ratelimitAsync: () => {};
+	memeAsync: () => {};
+	catsAsync: () => {};
+	animeAsync: () => {};
+	ramAsync: () => {};
+	nekoparaAsync: () => {};
+	version_checkAsync: () => {};
+	rpsAsync: () => {};
+}
+
+declare class Utils {
+	constructor();
+
+	custom: (
+		endpoint: string,
+		version: string,
+		Options?: object,
+	) => {
+		endpoint: string;
+		version: string;
+		Options?: object | null;
+	};
+
+	ping: () => {};
+}
+
+declare class RamApiBasic {
+	constructor(apikey: sting, version: string);
+
+	helloAsync: (lang?: string) => {
+		lang?: sting | null;
+	};
+	_8ballAsync: (lang?: sting | null) => {
+		lang?: sting | null;
+	};
+	cuddleAsync: () => {};
+	goodmorningAsync: (lang?: string) => {
+		lang?: string;
+	};
+
+	goodnightAsync: (lang?: string) => {
+		lang?: string;
+	};
+	hugAsync: () => {};
+	kissAsync: () => {};
+	slapAsync: () => {};
+	sickAsync: () => {};
+	tiredAsync: () => {};
+	cryAsync: (lang?: string) => {
+		lang?: string;
+	};
+	laughAsync: () => {};
+	birthdayAsync: () => {};
+	version_infoAsync: () => {};
+	ratelimitAsync: () => {};
+	memeAsync: () => {};
+	catsAsync: () => {};
+	animeAsync: () => {};
+	ramAsync: () => {};
+	nekoparaAsync: () => {};
+	version_checkAsync: () => {};
+}
