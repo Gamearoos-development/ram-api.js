f7989d6f59f870ca322768a0deccfdda9b6ca1f4
Gamearoo Codes <gamearoo10@gmail.com>
2023-06-19 14:08:13 -0400

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,9 @@ let apiutils = new Utils();
 
 
 async function test() {
-    console.log(await apiutils.customAsync('/basic/v13/versionCheck').catch(err => console.log(err)))
+    console.log(await apiutils.customAsync('/basic/v13/versionCheck').catch(err => console.log(err)));
+
+    console.log("DONE");
 }
 
 test()
