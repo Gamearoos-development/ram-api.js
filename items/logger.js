b9f45aaa033771e9a5b17bd69c495f97b5edd803
Gamearoo Codes <gamearoo10@gmail.com>
2023-08-03 16:11:45 -0400

    changes to logger

---
+++ items/logger.js
@@ -0,0 +1,74 @@
+class ExecuteConsole {
+  /**
+   *
+   * @param {String} name
+   * @param {{timezone, format, Logcustom, logDir}} _options
+   */
+  constructor(name, _options) {
+    if (!logDir) logDir = "./ram-api-logs";
+
+    if (!timezone) timezone = "America/New_York";
+
+    if (!Logcustom)
+      Logcustom = {
+        character: "*",
+        gray: "#bbbbbb",
+        namecolor: "#ff0000",
+        processcolor: "#ff6400",
+        titlecolor: "#00ff78",
+        textcolor: "#00ff8f",
+        datecolor: "#00a6ff",
+      };
+
+    let writeLogs = true;
+
+    if (!logDir) writeLogs = false;
+
+    let templog = new Logger({
+      name,
+      timezone,
+      tzformat: format,
+      dirpath: logDir,
+      writeLogs: true,
+      colored: true,
+      custom: Logcustom,
+    });
+    this.log = templog;
+
+    logger = new Logger({
+      name: `ram-api.js`,
+      timezone,
+      tzformat: format,
+      dirpath: logDir,
+      writeLogs: true,
+      colored: true,
+      custom: Logcustom,
+    });
+  }
+
+  /**
+   *
+   * @param {String} msg
+   */
+  errorAsync(msg) {
+    this.log.error(msg);
+  }
+
+  /**
+   *
+   * @param {String} msg
+   */
+  infoAsync(msg) {
+    this.log.info(msg);
+  }
+
+  /**
+   *
+   * @param {String} msg
+   */
+  warnAsync(msg) {
+    this.log.warn(msg);
+  }
+}
+
+module.exports = { ExecuteConsole };
