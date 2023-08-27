77dd28649b02df63fc67b225be808f5929367dde
gamearoo_codes <50187502+GamearooCodes@users.noreply.github.com>
2023-08-27 15:24:41 -0400

    f

--- test.js
+++ test.js
@@ -1,15 +1,10 @@
 const { RamApiAsync, RamApi, Utils } = require("./index");
 
-let apiclient = new RamApi('COlCbscBPr', "v13")
+let apiclient = new RamApi("COlCbscBPr", "v13");
 let apiutils = new Utils();
 
-
-
 async function test() {
-    console.log(await apiutils.customAsync('/basic/v13/versionCheck').catch(err => console.log(err)));
-
-    console.log("DONE");
+  console.log("DONE");
 }
 
-test()
-
+test();
