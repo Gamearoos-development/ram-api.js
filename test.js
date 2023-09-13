08dbfbe0fbe85dbf7ca1320d273f7ed27af73600
gamearoo_codes <50187502+GamearooCodes@users.noreply.github.com>
2023-09-12 23:33:23 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,3 @@
-const { RamApiAsync, RamApi, Utils } = require("./index");
-
-let apiclient = new RamApi("COlCbscBPr", "v13");
-let apiutils = new Utils();
-
 async function test() {
   console.log("DONE");
 }
