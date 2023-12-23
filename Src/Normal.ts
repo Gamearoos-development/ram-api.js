1b219eeba7824a46dad982902ef1432606f64248
GamearooCodes <50187502+GamearooCodes@users.noreply.github.com>
2023-12-23 04:16:58 -0500

    s

--- Src/Normal.ts
+++ Src/Normal.ts
@@ -5,236 +5,243 @@ import { ExecuteLogger } from "./ExecuteLoger";
 
 
 var logger = new ExecuteLogger("Ram Bot Development");
-  export class NormalEndpoints {
-    private retryAfter;
-    private retry;
-    private baseURL;
-    private version;
-    private headers;
-   
-    
-   
-    constructor(apikey: string, version = "v14", retryAfter = 60000, retry = 6) {
-      logger = new ExecuteLogger("Ram Bot Development");
-        this.retryAfter = retryAfter;
-        this.retry = retry;
-        this.baseURL = "https://api.rambot.xyz/demo"
-        this.version = version;
-        this.headers = {"api-key": apikey};
-    }
-    private async _newErrors(endpoint: string, error: any, resolve: any, reject: any, type: string, _options: any, loop = 0) {
-       
-        if (error.response) {
-            let err = `Status: ${error.response.status} | Error: ${error.response.statusText}`;
-            logger.errorAsync(err, "ram-api");
-            if (
-              this.retry > 0 &&
-              error.response.statusText === "Too Many Requests" &&
-              this.retry >= loop &&
-              type === "get"
-            ) {
-              logger.warnAsync(`api rate limit reached Retrying to run ${endpoint}`, "ram-api.js");
-      
-              setTimeout(async () => {
-      
-                try {
-                  const response = await axios({
-                    method: "get",
-                    url: endpoint,
-                    params: _options.params || {},
-                    baseURL: this.baseURL,
-                    headers: _options.headers || {},
-                  });
-      
-                  resolve(response.data);
-                } catch (err) {
-                  loop++;
-                  this._newErrors(
-                    endpoint,
-                    err,
-                    resolve,
-                    reject,
-                    type,
-                    _options,
-                    loop
-                  );
-                }
-              }, this.retryAfter);
-            } else {
-              if (error.response.data.error.message)
-                logger.errorAsync(
-                  `Ram Api Ran into an error while running ${endpoint}. The problem is: ${error.response.data.error.message}.`, "ram-api.js"
-                );
-              reject("Error Handled By Ram-api.js");
-            }
-          } else {
-           
-            console.log(error);
-            logger.errorAsync(
-              `Error running ${endpoint}. Please report to the developers the error logged into your console!`, "ram-api.js"
-            );
-            reject("Error Handled By Ram-api.js");
-            
-            
-          }
-    }
+export class NormalEndpoints {
+  private retryAfter;
+  private retry;
+  private baseURL;
+  private version;
+  private headers;
 
-    private _apiRequest(
-        endpoint: string,
-        _options?: {
-           params?: {}
-}
-    ) {
-      
-        return new Promise(async (resolve, reject) => {
 
-           _options = _options || {};
-          
-            const supportedVersions = ["v14"];
-            const outdatedVersions = ["v11", "v12", "v13"];
-            const notsupported = [
-              "v0",
-              "v1",
-              "v2",
-              "v3",
-              "v4",
-              "v5",
-              "v6",
-              "v7",
-              "v8",
-              "v9",
-              "v10",
-            ];
-            const requestedVersion = this.version || "v14";
-      
-            if (supportedVersions.includes(requestedVersion)) {
-            } else if (outdatedVersions.includes(requestedVersion)) {
-              logger.warnAsync(
-                `${requestedVersion} is outdated please update to the latest ${supportedVersions[0]}`, "ram-api.js"
-              );
-            } else if (notsupported.includes(requestedVersion)) {
-              logger.errorAsync(`${requestedVersion} is no longer supported`, "ram-api.js");
-              return reject("Check console");
-            }
-      
-      
-               
-      
-            try {
-              const response = await axios({
-                method: "get",
-                url: endpoint,
-                params: _options.params || {},
-                baseURL: this.baseURL,
-                headers: this.headers
-              });
-      
-              resolve(response.data);
-            } catch (error) {
-              this._newErrors(
-                endpoint,
-                error,
-                resolve,
-                reject,
-                "get",
-                _options
-              );
-            }
-          });
-    }
-    async versionInfoAsync(): Promise<{ version: string; outdated: boolean; supported: boolean; latest: string }> {
-    
-    
-      return await this._apiRequest("/versionCheck") as Promise<{ version: string; outdated: boolean; supported: boolean; latest: string }>;
-    }
 
-    async birthdayAsync(lang = "english"): Promise<{ text: string, url: string }> {
-        return await this._apiRequest("/bday", {
-            params: { lang }
-        }) as Promise<{ text: string, url: string }>;
-      }
-      async laughAsync(): Promise<{ url: string }> {
-        return await this._apiRequest("/laugh") as Promise<{ url: string }>;
-      }
-      async cryAsync(lang = "english"): Promise<{ text: string, url: string }> {
-        return await this._apiRequest("/cry", { params: { lang } }) as Promise<{ text: string, url: string }>;
-      }
-      async tiredAsync(): Promise<{ url: string }> {
-        return await this._apiRequest("/tired") as Promise<{ url: string }>;
-      }
-      async sickAsync(): Promise<{ url: string }> {
-        return await this._apiRequest("/sick") as Promise<{ url: string }>;
-      }
-      
-      async slapAsync(user1 = "You", user2 = "Ram"): Promise<{ text: string, url: string, text2: string, text3: string, text4: string, textFormatted: string }> {
-        return await this._apiRequest("/slap",  {
-          params: { user1, user2},
-        }) as Promise<{ text: string, url: string, text2: string, text3: string, text4: string, textFormatted: string }>;
-      }
-      async kissAsync(): Promise<{ url: string }> {
-        return await this._apiRequest("/kiss") as Promise<{ url: string }>;
-      }
-      async hugAsync(): Promise<{ url: string }> {
-        return await this._apiRequest("/hug") as Promise<{ url: string }>;
-      }
-      async goodNightAsync(lang = "english"): Promise<{ text: string, url: string }> {
-        return await this._apiRequest("/goodNight") as Promise<{ text: string, url: string }>;
-      }
-      async goodMorningAsync(lang = "english"): Promise<{ text: string, url: string }> {
-        return await this._apiRequest("/goodMorning", { params: { lang } }) as Promise<{ text: string, url: string }>;
-      }
-      async cuddleAsync(): Promise<{ url: string }> {
-        return await this._apiRequest("/cuddle") as Promise<{ url: string }>;
-      }
-      async helloAsync(lang = "english"): Promise<{ text: string}> {
-        return await this._apiRequest("/hello", { params: { lang } }) as Promise<{ text: string}>;
-      }
-      async rpsAsync(pick = "No Choice Made"): Promise<{ api_choice: string, your_choice: string }> {
-        return await this._apiRequest("/rps", { params: { pick } }) as Promise<{ api_choice: string, your_choice: string }>;
-      }
-      async _8ballAsync(lang = "english"): Promise<{ text: string }> {
-        return await this._apiRequest("/8ball", { params: { lang } }) as Promise<{ text: string }>;
-      }
-      async rateLimitAsync(): Promise<{ limit: number, current: number, remaining: number, resetTime: string, resetTimeFormated: string  }> {
-        return await this._apiRequest("/ratelimit") as Promise<{ limit: number, current: number, remaining: number, resetTime: string, resetTimeFormated: string  }>;
-      }
-      async randomNumberAsync(min: number, max: number): Promise<{ Number: number, roundedNumber: number, flooredNumber: number, enteredNumbers: string}> {
-        return await this._apiRequest("/randomNumber", {
-          params: { min, max },
-        }) as Promise<{ Number: number, roundedNumber: number, flooredNumber: number, enteredNumbers: string}>;
-      }
-      async suggestionAsync(suggestion: string, requestedBy?: string): Promise<string> {
-        return new Promise(async (resolve, reject) => {
-          let params = {};
-      
-          if (requestedBy) params = { requestedBy };
-      
-          try {
-            await axios({
-              method: "post",
-              url: `/suggestion/${suggestion}`,
-              data: {}, // Replace {} with the actual request body you want to send
-              params: params || {},
-              baseURL: this.baseURL,
-              headers: this.headers
-            });
-            resolve("Completed!");
-          } catch (err) {
-            reject(err);
-            logger.errorAsync("Error in api", "ram-api.js");
-          }
-        });
-      }
-      async pingAsync(): Promise<{ping: string}> {
-        return await this._apiRequest("/ping") as Promise<{ping: string}>;
-      }
-      async ramAsync(): Promise<{url: string}> {
-        return await this._apiRequest("/ram") as Promise<{url: string}>;
-      }
-      async nekoparaAsync(): Promise<{url: string}> {
-        return await this._apiRequest("/nekopara") as Promise<{url: string}>;
-      }
-      async mikuAsync(): Promise<{url: string}> {
-        return await this._apiRequest("/miku") as Promise<{url: string}>;
-      }
+  constructor(apikey: string, version = "v14", retryAfter = 60000, retry = 6) {
+    logger = new ExecuteLogger("Ram Bot Development");
+    this.retryAfter = retryAfter;
+    this.retry = retry;
+    this.baseURL = "https://api.rambot.xyz/demo"
+    this.version = version;
+    this.headers = { "api-key": apikey };
   }
+  private async _newErrors(endpoint: string, error: any, resolve: any, reject: any, type: string, _options: any, loop = 0) {
+
+    if (error.response) {
+      let err = `Status: ${error.response.status} | Error: ${error.response.statusText}`;
+      logger.errorAsync(err, "ram-api");
+      if (
+        this.retry > 0 &&
+        error.response.statusText === "Too Many Requests" &&
+        this.retry >= loop &&
+        type === "get"
+      ) {
+        logger.warnAsync(`api rate limit reached Retrying to run ${endpoint}`, "ram-api.js");
+
+        setTimeout(async () => {
+
+          try {
+            const response = await axios({
+              method: "get",
+              url: endpoint,
+              params: _options.params || {},
+              baseURL: this.baseURL,
+              headers: _options.headers || {},
+            });
+
+            resolve(response.data);
+          } catch (err) {
+            loop++;
+            this._newErrors(
+              endpoint,
+              err,
+              resolve,
+              reject,
+              type,
+              _options,
+              loop
+            );
+          }
+        }, this.retryAfter);
+      } else {
+        if (error.response.data.error.message)
+          logger.errorAsync(
+            `Ram Api Ran into an error while running ${endpoint}. The problem is: ${error.response.data.error.message}.`, "ram-api.js"
+          );
+        reject("Error Handled By Ram-api.js");
+      }
+    } else {
+
+      console.log(error);
+      logger.errorAsync(
+        `Error running ${endpoint}. Please report to the developers the error logged into your console!`, "ram-api.js"
+      );
+      reject("Error Handled By Ram-api.js");
+
+
+    }
+  }
+
+  private _apiRequest(
+    endpoint: string,
+    _options?: {
+      params?: {}
+    }
+  ) {
+
+    return new Promise(async (resolve, reject) => {
+
+      _options = _options || {};
+
+      const supportedVersions = ["v15"];
+      const outdatedVersions = ["v12", "v13", "v14"];
+      const notsupported = [
+        "v0",
+        "v1",
+        "v2",
+        "v3",
+        "v4",
+        "v5",
+        "v6",
+        "v7",
+        "v8",
+        "v9",
+        "v10",
+        "v11"
+      ];
+      const requestedVersion = this.version || "v15";
+
+      if (supportedVersions.includes(requestedVersion)) {
+      } else if (outdatedVersions.includes(requestedVersion)) {
+        logger.warnAsync(
+          `${requestedVersion} is outdated please update to the latest ${supportedVersions[0]}`, "ram-api.js"
+        );
+      } else if (notsupported.includes(requestedVersion)) {
+        logger.errorAsync(`${requestedVersion} is no longer supported`, "ram-api.js");
+        return reject("Check console");
+      }
+
+
+
+
+      try {
+        const response = await axios({
+          method: "get",
+          url: endpoint,
+          params: _options.params || {},
+          baseURL: this.baseURL,
+          headers: this.headers
+        });
+
+        resolve(response.data);
+      } catch (error) {
+        this._newErrors(
+          endpoint,
+          error,
+          resolve,
+          reject,
+          "get",
+          _options
+        );
+      }
+    });
+  }
+  async versionInfoAsync(): Promise<{ version: string; outdated: boolean; supported: boolean; latest: string }> {
+
+
+    return await this._apiRequest("/versionCheck") as Promise<{ version: string; outdated: boolean; supported: boolean; latest: string }>;
+  }
+
+  async memeAsync(): Promise<{ url: string }> {
+    return await this._apiRequest("/meme", {
+
+    }) as Promise<{ url: string }>;
+  }
+
+  async birthdayAsync(lang = "english"): Promise<{ text: string, url: string }> {
+    return await this._apiRequest("/bday", {
+      params: { lang }
+    }) as Promise<{ text: string, url: string }>;
+  }
+  async laughAsync(): Promise<{ url: string }> {
+    return await this._apiRequest("/laugh") as Promise<{ url: string }>;
+  }
+  async cryAsync(lang = "english"): Promise<{ text: string, url: string }> {
+    return await this._apiRequest("/cry", { params: { lang } }) as Promise<{ text: string, url: string }>;
+  }
+  async tiredAsync(): Promise<{ url: string }> {
+    return await this._apiRequest("/tired") as Promise<{ url: string }>;
+  }
+  async sickAsync(): Promise<{ url: string }> {
+    return await this._apiRequest("/sick") as Promise<{ url: string }>;
+  }
+
+  async slapAsync(user1 = "You", user2 = "Ram"): Promise<{ text: string, url: string, text2: string, text3: string, text4: string, textFormatted: string }> {
+    return await this._apiRequest("/slap", {
+      params: { user1, user2 },
+    }) as Promise<{ text: string, url: string, text2: string, text3: string, text4: string, textFormatted: string }>;
+  }
+  async kissAsync(): Promise<{ url: string }> {
+    return await this._apiRequest("/kiss") as Promise<{ url: string }>;
+  }
+  async hugAsync(): Promise<{ url: string }> {
+    return await this._apiRequest("/hug") as Promise<{ url: string }>;
+  }
+  async goodNightAsync(lang = "english"): Promise<{ text: string, url: string }> {
+    return await this._apiRequest("/goodNight") as Promise<{ text: string, url: string }>;
+  }
+  async goodMorningAsync(lang = "english"): Promise<{ text: string, url: string }> {
+    return await this._apiRequest("/goodMorning", { params: { lang } }) as Promise<{ text: string, url: string }>;
+  }
+  async cuddleAsync(): Promise<{ url: string }> {
+    return await this._apiRequest("/cuddle") as Promise<{ url: string }>;
+  }
+  async helloAsync(lang = "english"): Promise<{ text: string }> {
+    return await this._apiRequest("/hello", { params: { lang } }) as Promise<{ text: string }>;
+  }
+  async rpsAsync(pick = "No Choice Made"): Promise<{ api_choice: string, your_choice: string }> {
+    return await this._apiRequest("/rps", { params: { pick } }) as Promise<{ api_choice: string, your_choice: string }>;
+  }
+  async _8ballAsync(lang = "english"): Promise<{ text: string }> {
+    return await this._apiRequest("/8ball", { params: { lang } }) as Promise<{ text: string }>;
+  }
+  async rateLimitAsync(): Promise<{ limit: number, current: number, remaining: number, resetTime: string, resetTimeFormated: string }> {
+    return await this._apiRequest("/ratelimit") as Promise<{ limit: number, current: number, remaining: number, resetTime: string, resetTimeFormated: string }>;
+  }
+  async randomNumberAsync(min: number, max: number): Promise<{ Number: number, roundedNumber: number, flooredNumber: number, enteredNumbers: string }> {
+    return await this._apiRequest("/randomNumber", {
+      params: { min, max },
+    }) as Promise<{ Number: number, roundedNumber: number, flooredNumber: number, enteredNumbers: string }>;
+  }
+  async suggestionAsync(suggestion: string, requestedBy?: string): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      let params = {};
+
+      if (requestedBy) params = { requestedBy };
+
+      try {
+        await axios({
+          method: "post",
+          url: `/suggestion/${suggestion}`,
+          data: {}, // Replace {} with the actual request body you want to send
+          params: params || {},
+          baseURL: this.baseURL,
+          headers: this.headers
+        });
+        resolve("Completed!");
+      } catch (err) {
+        reject(err);
+        logger.errorAsync("Error in api", "ram-api.js");
+      }
+    });
+  }
+  async pingAsync(): Promise<{ ping: string }> {
+    return await this._apiRequest("/ping") as Promise<{ ping: string }>;
+  }
+  async ramAsync(): Promise<{ url: string }> {
+    return await this._apiRequest("/ram") as Promise<{ url: string }>;
+  }
+  async nekoparaAsync(): Promise<{ url: string }> {
+    return await this._apiRequest("/nekopara") as Promise<{ url: string }>;
+  }
+  async mikuAsync(): Promise<{ url: string }> {
+    return await this._apiRequest("/miku") as Promise<{ url: string }>;
+  }
+}
--- Src/Utils.ts
+++ Src/Utils.ts
@@ -3,10 +3,10 @@ import { ExecuteLogger } from "../Src/ExecuteLoger";
 import { DemoEndpoints } from "./Demo";
 
 var logger = new ExecuteLogger("Ram Bot Development");
-const latestVersion = "v14";
+const latestVersion = "v15";
 
-const supportedVersions = ["v14"];
-const outdatedVersions = ["v11", "v12", "v13"];
+const supportedVersions = ["v15"];
+const outdatedVersions = ["v12", "v13", "v14"];
 const notsupported = [
   "v0",
   "v1",
@@ -19,144 +19,145 @@ const notsupported = [
   "v8",
   "v9",
   "v10",
+  'v11'
 ];
 
 
 
 export class Utils {
-    constructor() {
-      logger = new ExecuteLogger("Ram Bot Development");
+  constructor() {
+    logger = new ExecuteLogger("Ram Bot Development");
+  }
+  async pingAsync(): Promise<{ ping: string }> {
+
+
+    return new Promise(async (resolve, reject) => {
+      await axios
+        .get(`/ping`, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          baseURL: `https://api.rambot.xyz/basic/v12/utils`,
+        })
+        .then(async function (res) {
+          resolve(res.data);
+        })
+        .catch(async (error) => {
+          reject("Ping Failed! " + error);
+        });
+    });
+  }
+  async customAsync(endpoint: string, accessLevel = "demo", _options: { version?: string, params?: {}, headers?: {} }): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+
+      let requestedVersion = `${_options.version}` || latestVersion;
+
+      if (supportedVersions.includes(requestedVersion)) {
+      } else if (outdatedVersions.includes(requestedVersion)) {
+        logger.warnAsync(
+          `${requestedVersion} is outdated please update to the latest ${supportedVersions[0]}`, "ram-api.js"
+        );
+      } else if (notsupported.includes(requestedVersion)) {
+        logger.errorAsync(`${requestedVersion} is no longer supported`, "ram-api.js");
+        return reject("Check console");
+      }
+
+      const basicVersions = ["v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13"]
+
+
+
+      if (accessLevel === "basic" && !basicVersions.includes(requestedVersion)) {
+        logger.errorAsync("Basic endpoints where removed in v14 and above!", "ram-api");
+
+        return reject("ram-api ran into a error")
+      }
+
+
+      if (accessLevel === "demo" && requestedVersion !== latestVersion) {
+        logger.errorAsync(`Demo Doesn't support older versions you must use the latest ${latestVersion}!`, "ram-api");
+
+        return reject("ram-api ran into a error")
+      }
+
+      let baseURL = "NULL";
+
+      if (accessLevel === "normal") {
+        baseURL = `https://api.rambot.xyz/${requestedVersion}`;
+      } else if (accessLevel === "extended") {
+        baseURL = `https://api.rambot.xyz/extended/${requestedVersion}`;
+      } else if (accessLevel === "demo") {
+        baseURL = "https://api.rambot.xyz/demo";
+      } else if (accessLevel === "pro") {
+        baseURL = `https://api.rambot.xyz/pro/${requestedVersion}`;
+      } else if (accessLevel === "basic") {
+        baseURL = `https://api.rambot.xyz/basic/${requestedVersion}`;
+      }
+
+      try {
+        const response = await axios({
+          method: "get",
+          url: endpoint,
+          params: _options.params || {},
+          baseURL: baseURL,
+          headers: _options.headers || {},
+        });
+
+        resolve(response.data);
+
+
+
+      } catch (error: any) {
+
+        if (error.response) {
+          let err = `Status: ${error.response.status} | Error: ${error.response.statusText}`;
+          logger.errorAsync(err, "ram-api");
+          logger.errorAsync(error, "ram-api.js");
+          reject(error);
+
+        } else {
+          console.log(error);
+          logger.fatalAsync(
+            `Unhandled Error running ${baseURL + endpoint}. Please report to the developers the error logged into your console!`, "ram-api.js"
+          );
+
+        }
+
+      }
+    });
+  }
+
+  async VersionCheck(version?: string, retryAfter = 60000, retry = 6) {
+
+    if (!version) {
+      version = latestVersion
+      logger.warnAsync("Version not provide in VersionCheck So using the latest version", "ram-api.js")
     }
-    async pingAsync(): Promise<{ping: string}> {
- 
-    
-       return new Promise(async (resolve, reject) => {
-          await axios
-            .get(`/ping`, {
-              headers: {
-                "Content-Type": "application/json",
-              },
-              baseURL: `https://api.rambot.xyz/basic/v12/utils`,
-            })
-            .then(async function (res) {
-              resolve(res.data);
-            })
-            .catch(async (error) => {
-              reject("Ping Failed! " + error);
-            });
-        });
-      }
-      async customAsync(endpoint: string, accessLevel = "demo", _options: {version?: string, params?: {}, headers?: {}}): Promise<any> {
-        return new Promise(async (resolve, reject) => {
-         
-         let requestedVersion = `${_options.version}` || latestVersion;
-    
-          if (supportedVersions.includes(requestedVersion)) {
-          } else if (outdatedVersions.includes(requestedVersion)) {
-            logger.warnAsync(
-              `${requestedVersion} is outdated please update to the latest ${supportedVersions[0]}`, "ram-api.js"
-            );
-          } else if (notsupported.includes(requestedVersion)) {
-            logger.errorAsync(`${requestedVersion} is no longer supported`, "ram-api.js");
-            return reject("Check console");
-          }
 
-          const basicVersions = ["v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13"]
+    new DemoEndpoints(retryAfter, retry).versionInfoAsync(version).then(data => {
+      if (data.outdated) {
+        if (data.supported) {
+          logger.warnAsync(`${data.version} is outdated but is still supported latest is ${data.latest}`, "ram-api.js");
 
-       
-
-          if(accessLevel === "basic" && !basicVersions.includes(requestedVersion) ) {
-            logger.errorAsync("Basic endpoints where removed in v14 and above!", "ram-api");
-
-            return reject("ram-api ran into a error")
-          }
-
-          
-          if(accessLevel === "demo" && requestedVersion !== latestVersion ) {
-            logger.errorAsync(`Demo Doesn't support older versions you must use the latest ${latestVersion}!`, "ram-api");
-
-            return reject("ram-api ran into a error")
-          }
-    
-          let baseURL = "NULL";
-    
-          if (accessLevel === "normal") {
-            baseURL = `https://api.rambot.xyz/${requestedVersion}`;
-          } else if (accessLevel === "extended") {
-            baseURL = `https://api.rambot.xyz/extended/${requestedVersion}`;
-          } else if (accessLevel === "demo") {
-            baseURL = "https://api.rambot.xyz/demo";
-          } else if (accessLevel === "pro") {
-            baseURL = `https://api.rambot.xyz/pro/${requestedVersion}`;
-          } else if (accessLevel === "basic") {
-            baseURL = `https://api.rambot.xyz/basic/${requestedVersion}`;
-          }
-    
-          try {
-            const response = await axios({
-              method: "get",
-              url: endpoint,
-              params: _options.params || {},
-              baseURL: baseURL,
-              headers: _options.headers || {},
-            });
-    
-            resolve(response.data);
-
-    
-                
-            } catch (error: any) {
-            
-            if (error.response) {
-                let err = `Status: ${error.response.status} | Error: ${error.response.statusText}`;
-            logger.errorAsync(err, "ram-api");
-            logger.errorAsync(error, "ram-api.js");
-            reject(error);
-            
-            } else {
-                console.log(error);
-                logger.fatalAsync(
-                  `Unhandled Error running ${baseURL + endpoint}. Please report to the developers the error logged into your console!`, "ram-api.js"
-                );
-                
-            }
-            
-          }
-        });
+          return
+        }
+        logger.errorAsync(`${data.version} is outdated and is no longer supported please update to latest ${data.latest}`, "ram-api.js")
+        return
       }
 
-      async VersionCheck( version?: string, retryAfter = 60000, retry = 6) {
-    
-        if(!version) {
-            version = latestVersion
-            logger.warnAsync("Version not provide in VersionCheck So using the latest version", "ram-api.js")
-        }
+      if (data.latest === version) {
+        logger.infoAsync("You're Up to date", "ram-api.js")
+      } else {
+        logger.fatalAsync("Error fetching provided version", "ram-api")
+      }
+    }).catch(err => { })
 
-        new DemoEndpoints(retryAfter, retry).versionInfoAsync(version).then(data => {
-            if(data.outdated) {
-                if(data.supported) {
-                    logger.warnAsync(`${data.version} is outdated but is still supported latest is ${data.latest}`, "ram-api.js");
 
-                    return
-                }
-                    logger.errorAsync(`${data.version} is outdated and is no longer supported please update to latest ${data.latest}`, "ram-api.js")
-                return
-            }
 
-            if(data.latest === version) {
-                logger.infoAsync("You're Up to date", "ram-api.js")
-            } else {
-                logger.fatalAsync("Error fetching provided version", "ram-api")
-            }
-        }).catch(err => {})
 
-        
 
-            
-                
-            
-        }
 
-        
-      
+  }
+
+
+
 }
